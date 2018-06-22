ac374f37e1cd79001d91a2e0b5aef38dcc00b143
llrs <lluis.revilla@gmail.com>
2018-06-22 15:33:43 +0200

    Improving? imports

--- R/integration-package.R
+++ R/integration-package.R
@@ -2,14 +2,15 @@
 #' @import stats
 #' @import utils
 #' @import ggplot2
-#' @import RGCCA
-#' @import fgsea
+#' @importFrom RGCCA sgcca tau.estimate rgcca cov2
+#' @importFrom fgsea fgsea
 #' @import reactome.db
 #' @import org.Hs.eg.db
-#' @import data.table
-#' @import clusterProfiler
+#' @importFrom data.table setorder
+#' @importFrom clusterProfiler enricher
 #' @import ReactomePA
-#' @import  AnnotationDbi
+#' @importFrom AnnotationDbi select mapIds
+#' @importFrom grDevices pdf dev.off
 NULL
 
 
