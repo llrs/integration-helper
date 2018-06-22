4de0aed02f99f41f58dd9a4d51e8b4845b2159a8
llrs <lluis.revilla@gmail.com>
2018-06-22 17:03:30 +0200

    Adding a function to clean the epithelium

---
+++ R/epithelium.R
@@ -0,0 +1,16 @@
+#' Function to read the epithelium signature
+#'
+#' Return the Epithelium in Entrez ids
+#' @param x A character vector with SYMBOL names
+#' @return A character vector with the Entrez ids
+#' @export
+epitheliumE <- function(x){
+  epitheliumE <- AnnotationDbi::mapIds(
+    org.Hs.eg.db::org.Hs.eg.db,
+    keys = as.character(x),
+    keytype = "SYMBOL", column = "ENTREZID"
+  )
+
+  epitheliumE <- unlist(epitheliumE, use.names = TRUE)
+  epitheliumE[!is.na(epitheliumE)]
+}
