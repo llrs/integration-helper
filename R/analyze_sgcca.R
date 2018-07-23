cbc8124f7fa0217682a91531f380410e8d711e80
llrs <lluis.revilla@gmail.com>
2018-07-23 16:28:37 +0200

    Improve internals

--- R/analyze_sgcca.R
+++ R/analyze_sgcca.R
@@ -8,25 +8,41 @@
 #' the number of interactions that exists.
 #' @export
 analyze <- function(sgcca) {
-  ind <- apply(which(upper.tri(result.sgcca$C), arr.ind = TRUE), 1,
-               paste0, collapse = "")
+  ind <- index(sgcca)
 
-  # Correlation between Y
-  Y <- simplify2array(result.sgcca$Y, higher = FALSE)
-  cY <- cor(Y)
-  d <- cY * x
+  cY <- canonical_correlation(sgcca)
+  cc <- helper_cc(sgcca, cY)
+
+  # Values of the correlation with the dimensions
   var <- cY[upper.tri(cY)]
   names(var) <- paste0("vs", ind)
-  # Canonical correlation
-  cc <- sum(d[upper.tri(d)])
+
   # Values of the design matrix
-  vars <- result.sgcca$C[upper.tri(result.sgcca$C)]
+  vars <- sgcca$C[upper.tri(sgcca$C)]
   names(vars) <- paste0("var", ind)
+
   # weights used
   weight <- sum(vars != 0)
   names(weight) <- "weights"
 
   # Output
-  c(var, unlist(result.sgcca$AVE[c("AVE_inner", "AVE_outer")]), cc1 = cc,
+  c(var, unlist(sgcca$AVE[c("AVE_inner", "AVE_outer")]), cc1 = cc,
     vars, weight)
+}
+
+canonical_correlation <- function(sgcca) {
+  # Correlation between Y
+  Y <- simplify2array(sgcca$Y, higher = FALSE)
+  cor(Y)
+}
+
+helper_cc <- function(sgcca, cY) {
+  d <- cY * sgcca$C
+  # Canonical correlation
+  sum(d[upper.tri(d)])
+}
+
+index <- function(x) {
+  apply(which(upper.tri(x$C), arr.ind = TRUE), 1,
+        paste0, collapse = "")
 }
