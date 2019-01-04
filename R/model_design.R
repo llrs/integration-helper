60e38b61dd055584c2caa53eae2b61684adcb4c0
llrs <lluis.revilla@gmail.com>
2019-01-04 11:48:08 +0100

    Add a parameter for model_design for variations on only few positions of the matrix.

--- R/model_design.R
+++ R/model_design.R
@@ -2,21 +2,38 @@
 #' Check a design for different weights
 #' @param weights A numeric value with the number of weights to be used
 #' @param size A numveric value with the numer of datasets on the design.
+#' @param diff0 A Numeric vector of position which should be different from 0 from the lower.tri
 #' @return A list of matrices with the designs with different weights
 #' @export
 #' @author Flodel \url{https://codereview.stackexchange.com/a/203517/36067}
 #' @examples
 #' out <- weight_design(4, 4)
 #' head(out)
-weight_design <- function(weights = 4, size){
+weight_design <- function(weights = 4, size, diff0 = NULL){
 
   p <- size * (size - 1) / 2    # 6
   w <- seq(from = 0, to = 1, length.out = weights)
-
-  # all possible combinations by doing:
-  W <- as.matrix(expand.grid(rep(list(w), p)))
-
   X <- matrix(1:(size*size), size, size) # pattern matrix of indices
+
+  if (!is.null(diff0)) {
+    W <- as.matrix(expand.grid(rep(list(w), length(diff0))))
+    W0 <- matrix(0, nrow = nrow(W), ncol = p)
+    possible <- which(lower.tri(X))
+    keep <- possible %in% diff0
+    if (!any(keep)) {
+      possible <- which(upper.tri(X))
+      keep <- possible %in% diff0
+    }
+    if (!any(keep)) {
+      stop("Incorrect indices in diff0")
+    }
+    W0[, keep] <- W
+    W <- W0
+  } else {
+    # all possible combinations by doing:
+    W <- as.matrix(expand.grid(rep(list(w), p)))
+  }
+
   A <- matrix(0, nrow(W), size * size)
 
   # Replace the positions by the weights
